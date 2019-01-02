60741199755302a950b993c524e95ba4602a49c8
MrMedici <mr.medici@foxmail.com>
2019-01-02 14:11:00 +0800

    lqmooc:字符串文案修改

--- intl-education/src/main/java/com/lqwawa/intleducation/factory/constant/Constant.java
+++ intl-education/src/main/java/com/lqwawa/intleducation/factory/constant/Constant.java
@@ -2,8 +2,8 @@ package com.lqwawa.intleducation.factory.constant;
 
 public interface Constant {
 
-    String LQWAWA_COURSE = "【两栖习课程】";
-    String LQWAWA_ONLINE_COURSE = "【两栖名师课程】";
+    String LQWAWA_COURSE = "【两栖习教辅】";
+    String LQWAWA_ONLINE_COURSE = "【两栖名师课】";
 
 
 }
