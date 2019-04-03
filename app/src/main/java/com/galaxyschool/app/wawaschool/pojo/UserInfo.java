35b6b386292752713a2c357f450926ce2ae1bb13
quxiaoyong <qu_xiaoyong@163.com>
2019-04-03 16:13:40 +0800

    区分线上和线下的角色信息

--- app/src/main/java/com/galaxyschool/app/wawaschool/pojo/UserInfo.java
+++ app/src/main/java/com/galaxyschool/app/wawaschool/pojo/UserInfo.java
@@ -43,7 +43,15 @@ public class UserInfo extends Model implements Serializable {
     private String UserId;
     private boolean IsAssistant;
     private boolean IsNewCreateUser;//是否新创建的用户
+    private String allRoles;
 
+    public String getAllRoles() {
+        return allRoles;
+    }
+
+    public void setAllRoles(String allRoles) {
+        this.allRoles = allRoles;
+    }
     public boolean isNewCreateUser() {
         return IsNewCreateUser;
     }
