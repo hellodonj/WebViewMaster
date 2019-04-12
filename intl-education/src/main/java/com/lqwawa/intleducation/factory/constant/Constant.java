3bed7646abf31b3399e73677a9c3fbee470f56a0
MrMedici <mr.medici@foxmail.com>
2019-04-12 14:09:34 +0800

    lqmooc:测试:习教辅字段改成习课程

--- intl-education/src/main/java/com/lqwawa/intleducation/factory/constant/Constant.java
+++ intl-education/src/main/java/com/lqwawa/intleducation/factory/constant/Constant.java
@@ -2,7 +2,7 @@ package com.lqwawa.intleducation.factory.constant;
 
 public interface Constant {
 
-    String LQWAWA_COURSE = "【两栖习教辅】";
+    String LQWAWA_COURSE = "【两栖习课程】";
     String LQWAWA_ONLINE_COURSE = "【两栖在线课堂】";
 
 
