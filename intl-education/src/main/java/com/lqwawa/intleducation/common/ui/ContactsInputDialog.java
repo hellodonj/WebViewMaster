6b5e7abbd789642c58971a6f26df9188ec06baf6
dongjiajia <jiajiadong11@gmail.com>
2019-07-16 11:41:17 +0800

    帮辅空间-批阅价格，输入多个0的限制

--- intl-education/src/main/java/com/lqwawa/intleducation/common/ui/ContactsInputDialog.java
+++ intl-education/src/main/java/com/lqwawa/intleducation/common/ui/ContactsInputDialog.java
@@ -1,15 +1,18 @@
 package com.lqwawa.intleducation.common.ui;
 
 import android.content.Context;
+import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.lqwawa.intleducation.R;
+import com.lqwawa.intleducation.base.utils.ToastUtil;
 
-public class ContactsInputDialog extends ContactsDialog{
+public class ContactsInputDialog extends ContactsDialog {
 
     public ContactsInputDialog(Context context, String title,
                                String inputBoxDefaultText, String inputBoxHintText,
@@ -48,21 +51,46 @@ public class ContactsInputDialog extends ContactsDialog{
         return null;
     }
 
-    public void setUnitDisplay(boolean isDisplay){
+    public void setUnitDisplay(boolean isDisplay) {
         TextView textView = (TextView) getContentView().findViewById(R.id.tv_unit);
-        if (isDisplay){
+        if (isDisplay) {
             textView.setVisibility(View.VISIBLE);
-        }else {
+        } else {
             textView.setVisibility(View.GONE);
         }
     }
 
 
-    public void setInputLimitNumber(int length){
+    public void setInputLimitNumber(int length) {
         EditText textView = (EditText) getContentView().findViewById(
                 R.id.contacts_dialog_content_text);
-        if (textView!=null){
+        if (textView != null) {
             textView.setFilters(new InputFilter[]{new InputFilter.LengthFilter(length)});
         }
     }
+
+    public void setInputLimit() {
+        EditText textView = (EditText) getContentView().findViewById(
+                R.id.contacts_dialog_content_text);
+        textView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (textView.getText().toString().matches("^0")) {//判断当前的输入第一个数是不是为0
+                    textView.setText("");
+                    ToastUtil.showToast(getContext(), R.string.label_price_tip);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
 }
